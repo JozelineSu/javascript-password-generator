4eef145efe32ddd6938780713b29c896665c3246
unknown <jozelinesuarez@gmail.com>
2023-08-27 16:07:54 -0500

    Put in confirm statements to ask the user, added criteria so password length is kept between 8-128 and at lease one character option is chosen. And created an empty array for the new password

--- Develop/script.js
+++ Develop/script.js
@@ -1,54 +1,44 @@
-/*
-1. variables for length, lowercase, uppercase, numeric, special characters
-2. create prompts for each varibale for the user to answer
-3. validate users answers
-4. password is generated
-4. password is shown on screen
-
-What length do you want your password to be? (8 - 128 characters)
-  if length < 8 input is invalid
-    give error message
-  if length length === 8 && length >= 128 input is valid
-    proceed to next prompt
-Do you want to include lowercase letters? 
-  if yes proceed
-  if no proceed
-  same for the rest of the charactaer types
-  if no characters types are selected user should get an alert
-  "At least one character type should be selected to create a password. Please try again."
-  return to screen with button
-*/
-
-// Assignment code here
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 var length = 8;
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var number = 8;
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ];
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "?", "/", "[", "]", "{", "}", "<", ">", "."];
-
+var newPassword = [];  
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password; // the var password holds a generatePassword function the var passwordText connects us to the password id in index html, and this line gives the vale of password text equal to the password var
-
-}
+  passwordText.value = password;
+};
 
 function generatePassword() {
-  
+  var length = parseInt(prompt("What length do you want the password to be? (8 - 128 characters)"));
+    if (length < 8 || length > 128) {
+      alert("Please choose a number between 8 and 128");
+      return;
+    };
 
+  var lowerCase = confirm("Do you want lowercase letters included in your password? (Ok = yes & Cancel = no)");
+  var upperCase = confirm("Do you want UPPERCASE letters included in your password? (Ok = yes & Cancel = no)");
+  var number = confirm("Do you want to include numbers in your password? (Ok = yes & Cancel = no)");
+  var specialChar = confirm("Do you want to include special characters in your password? (Ok = yes & Cancel = no)");
 
+    if (lowerCase === false && upperCase === false && number === false && specialChar === false) {
+      alert("Please click 'Ok' for one option to generate a password.");
+      return;
+    };
 
+  if (lowerCase) {newPassword += lowerCase}
+  if (upperCase) {newPassword += upperCase;}
+  if (number) {newPassword += number;}
+  if (specialChar) {newPassword += specialChar;}
 
-
-
-}
+};
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); //when we clickon generate pasword, writePassword function will play out
+generateBtn.addEventListener("click", writePassword);
+
 
